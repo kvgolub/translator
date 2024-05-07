40144fc4abfc4f0932f31f3aba7bf7614dbbc7fd
kvgolub <110321029+kvgolub@users.noreply.github.com>
2024-05-07 09:03:18 +0200

    Add files via upload

---
+++ precache-manifest.dca29237a75428bb3b35ca485f89a4c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35a32eeeecbbcaa5aea1da33cb24250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8123388720d5457c8fcf",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e40ddf2b9b671016ed71",
+    "url": "/static/js/2.ef63edac.chunk.js"
+  },
+  {
+    "revision": "8123388720d5457c8fcf",
+    "url": "/static/js/main.322ff15f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
